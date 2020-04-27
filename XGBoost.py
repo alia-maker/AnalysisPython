5c03fa680fd289f3b2b287007a33c3d47baf1060
Aliya <alia921@mail.ru>
2020-04-28 00:00:02 +0300

    changes in xgboost

--- XGBoost.py
+++ XGBoost.py
@@ -3,28 +3,54 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pickle_data import pickle_it, unpickle_it
 import pandas as pd
+import dateparser
 # from sklearn.metrics import mean_absolute_error
 def PrepareData(data, test_size=0.15):
     # data = pd.DataFrame(data.copy()),
     # data.columns = ["y"]
        # считаем индекс в датафрейме, после которого начинается тестовыый отрезок"
     test_index = int(len(data)*(1-test_size))
-    # data.columns = ['y']
+    data.columns = ['y']
     # data = data.reset_index(drop=True)
     # data.astype(int)
-    #
+
     # print(data)
-    # exit()
+
+    # data['date'] = data.index
+    # print(data)
+    # data['hour'] = data['date'].dt.hour
+    # print(data)
+    # df['dayofweek'] = df['date'].dt.dayofweek
+    # df['quarter'] = df['date'].dt.quarter
+    # df['month'] = df['date'].dt.month
+    # df['year'] = df['date'].dt.year
+    # df['dayofyear'] = df['date'].dt.dayofyear
+    # df['dayofmonth'] = df['date'].dt.day
+    # df['weekofyear'] = df['date'].dt.weekofyear
+
     # Data = []
     # for d in data:
-    #     Data.extend(d.tolist())
+    #     data.extend(d.tolist())
 
-    # "    # добавляем лаги исходного ряда в качестве признаков"
-    # for i in range(lag_start, lag_end):
-    #     data["lag_{}".format(i)] = data.y.shift(i)
-    #
-    #     data.index = data.index.to_datetime()
-    #     data["hour"] = data.index.hour
+    "    # добавляем лаги исходного ряда в качестве признаков"
+    for i in range(1, 5):
+        temp_lag = data.y.shift(i)
+        # print(type(temp_lag[0]))
+        temp_lag[:i] = 0
+        # print(temp_lag)
+        # exit()
+        data["lag_{}".format(i)] = temp_lag
+
+    # print(type(data.index[0]))
+    # exit()
+    data.index = [dateparser.parse(x) for x in data.index]
+
+    data["minute"] = data.index.minute
+    data["hour"] = data.index.hour
+    data['quarter'] = data.index.quarter
+    data["weekday"] = data.index.weekday
+    # print(data)
+    # exit()
     # "    data[\"weekday\"] = data.index.weekday\n",
     # "    data['is_weekend'] = data.weekday.isin([5,6])*1\n",
     # "\n",
@@ -39,16 +65,24 @@ def PrepareData(data, test_size=0.15):
     # "    data = data.reset_index(drop=True)\n",
     # "\n",
     # разбиваем весь датасет на тренировочную и тестовую выборку\n",
-    # X_train = data.loc[:test_index].drop(["y"], axis=1)
-    X_train =(data[:test_index])
-    print(X_train)
-    # y_train = data.loc[:test_index]["y"]
-    y_train =(data.values[:test_index])
-    print(y_train)
+    # data = data.reset_index(drop=True)
+    # X_train = data[:test_index].drop(["y"], axis=1)
+    # print(X_train)
+
+    X_train = data[:test_index].drop(["y"], axis=1)
+    print('X_train', X_train)
+    X_test = data[test_index:].drop(["y"], axis=1)
+    print('X_test', X_test)
+
+    y_train = data[:test_index]["y"]
+    y_test = data[test_index:]["y"]
+    # print(y_train)
+    # print(y_test)
+    # exit()
     # X_test = data.loc[test_index:].drop(["y"], axis=1)
     # y_test = data.loc[test_index:]["y"]
-    X_test = (data[test_index + 20:])
-    y_test = (data.values[test_index + 20:])
+    # X_test = (data[test_index + 20:])
+    # y_test = (data.values[test_index + 20:])
 
     return X_train, X_test, y_train, y_test
 
@@ -66,7 +100,6 @@ def code_mean(data, cat_feature, real_feature):
     return dict(data.groupby(cat_feature)[real_feature].transform("mean"))
 
 def prepareData(data, lag_start=5, lag_end=20, test_size=0.15):
-
     data = pd.DataFrame(data.copy())
     data.columns = ["y"]
     "    # считаем индекс в датафрейме, после которого начинается тестовыый отрезок"
@@ -108,12 +141,17 @@ def XGB_forecast(data, test_size=0.15, lag_start=5, lag_end=30, scale=1.9):
     print('real data:', data)
     # исходные данные
     X_train, X_test, y_train, y_test = PrepareData(data)
-    print('X train', X_train)
+    print('X train', np.array(X_train.values).astype(float))
     print('y train', y_train)
-
-    dtrain = xgb.DMatrix(X_train, label=y_train)
     # exit()
-    dtest = xgb.DMatrix(X_test)
+
+    X = np.array(X_train.values).astype(float)
+    y = np.array(y_train.values).astype(float)
+    dtrain = xgb.DMatrix(X, label=y)
+    # exit()
+    XTest = np.array(X_test.values).astype(float)
+    ytest = np.array(y_test.values).astype(float)
+    dtest = xgb.DMatrix(XTest)
 
     # задаём параметры
     params = {
@@ -140,10 +178,16 @@ def XGB_forecast(data, test_size=0.15, lag_start=5, lag_end=30, scale=1.9):
     prediction_train = bst.predict(dtrain)
     plt.figure(figsize=(15, 5))
     # print(len(prediction_train))
+    # print(y_train)
+    # print(y_train.index)
+    # print(len(y_train.index))
+    # print(prediction_train)
+    # print(len(prediction_train))
+    # exit()
     plt.plot(y_train.index, prediction_train)
     # print(y_train)
     # exit()
-    plt.plot(y_train)
+    plt.plot(y_train.index, y)
     plt.axis('tight')
     plt.grid(True)
     # plt.show()
@@ -158,18 +202,21 @@ def XGB_forecast(data, test_size=0.15, lag_start=5, lag_end=30, scale=1.9):
     # print(lower)
     # exit()
     for i in range(len(y_test)):
-        if y_test.values[i] < lower[i]:
+        if float(y_test.values[i]) < lower[i]:
 
-            Anomalies[i] = y_test.values[i]
+            Anomalies[i] = float(y_test.values[i])
     for i in range(len(y_test)):
-        if y_test.values[i] > upper[i]:
+        if float(y_test.values[i]) > upper[i]:
 
-            Anomalies[i] = y_test.values[i]
+            Anomalies[i] = float(y_test.values[i])
     plt.figure(figsize=(15, 5))
+    print(y_test.index)
+    # exit()
     plt.plot(y_test.index, prediction_test, label="prediction")
     plt.plot(y_test.index, lower, "r--", label="upper/lower bound")
     plt.plot(y_test.index, upper, "r--")
-    plt.plot(y_test, label="y_test")
+
+    plt.plot(y_test.index, ytest, label="y_test")
     print(Anomalies)
     # exit()
     plt.plot(y_test.index, Anomalies, "ro", markersize=10, label="Anomalies")
@@ -192,6 +239,5 @@ if __name__=='__main__':
     # dtrain = xgb.DMatrix(data, label=label)
     # print(dtrain)
     # exit()
-
 
     XGB_forecast(dataset, test_size=0.2, lag_start=5, lag_end=30)
