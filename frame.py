7334b730ccdef8cbb350c0d4c50692b0b8beba85
Aliya <alia921@mail.ru>
2020-06-26 01:29:35 +0300

    deleted unwanted files

--- frame.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df2 = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [77, 78, 79]]),
-                   columns=['a', 'b', 'c'])
-
-print(df2)
-print(df2.columns)
